cd23480afd23b5a93782da970a19973ebd648d66
Shin seongsu <tlstjdtn321@naver.com>
2021-04-11 23:19:41 +0900

    jdbc 연결하여 Step 설정

--- ch7/src/main/java/org/spring/batch/configuration/ItemReaderConfiguration.java
+++ ch7/src/main/java/org/spring/batch/configuration/ItemReaderConfiguration.java
@@ -8,7 +8,10 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -17,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,11 +31,14 @@ public class ItemReaderConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory,
-                                   StepBuilderFactory stepBuilderFactory) {
+                                   StepBuilderFactory stepBuilderFactory,
+                                   DataSource dataSource) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -40,6 +47,7 @@ public class ItemReaderConfiguration {
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
                 .next(this.cvsFileStep())
+                .next(this.jdbcStep())
                 .build();
     }
 
@@ -59,6 +67,27 @@ public class ItemReaderConfiguration {
                 .reader(this.csvFileItemReader())
                 .writer(itemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step jdbcStep() throws Exception {
+        return stepBuilderFactory.get("jdbcStep")
+                .<Person, Person>chunk(10)
+                .reader(jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
+        JdbcCursorItemReader<Person> itemReader = new JdbcCursorItemReaderBuilder<Person>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .sql("select id, name, age, address from person")
+                .rowMapper((rs, rowNum) -> new Person(
+                        rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)))
+                .build();
+        itemReader.afterPropertiesSet();
+        return itemReader;
     }
 
     private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
