133b52f0e8d0afb4fded520a5c66dba886df45e5
Shin seongsu <tlstjdtn321@naver.com>
2021-04-19 09:40:54 +0900

    springbatch junit 수정

--- ch7/src/test/java/org/spring/batch/configuration/SavePersonConfigurationTest.java
+++ ch7/src/test/java/org/spring/batch/configuration/SavePersonConfigurationTest.java
@@ -1,6 +1,7 @@
 package org.spring.batch.configuration;
 
 import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.spring.batch.repository.PersonRepository;
@@ -9,6 +10,7 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,6 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBatchTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {SavePersonConfiguration.class, TestConfiguration.class})
 @EntityScan("org.spring.batch.dto")
@@ -28,6 +31,22 @@ public class SavePersonConfigurationTest {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        personRepository.deleteAll();
+    }
+
+    @Test
+    public void test_step() {
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("SavePersonStep");
+
+        Assertions.assertThat(jobExecution.getStepExecutions().stream()
+                        .mapToInt(StepExecution::getWriteCount)
+                        .sum())
+                        .isEqualTo(personRepository.count())
+                        .isEqualTo(3);
+    }
 
     @Test
     public void test_allow_duplicate() throws Exception {
@@ -47,6 +66,23 @@ public class SavePersonConfigurationTest {
                         .isEqualTo(3);
     }
 
+    @Test
+    public void test_not_allow_duplicate() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("allow_duplicate", "true")
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        Assertions.assertThat(jobExecution.getStepExecutions().stream()
+                .mapToInt(StepExecution::getWriteCount)
+                .sum())
+                .isEqualTo(personRepository.count())
+                .isEqualTo(100);
+    }
 
 
 }
